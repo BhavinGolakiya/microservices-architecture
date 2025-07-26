88675e7fc38dce22baca8122dc9d7f93d553dd17
BhavinGolakiya <bvgolakiya4@gmail.com>
2025-07-26 22:49:09 +0530

    update env

--- concurrency-test/check-socket.js
+++ concurrency-test/check-socket.js
@@ -1,19 +1,19 @@
-const { io } = require('socket.io-client')
+const { io } = require('socket.io-client');
 
 const socket = io('http://localhost:5000', {
-  path: '/socket',
-  transports: ['websocket'],
+  transports: ['websocket'],  // force WebSocket only (no polling)
+  reconnectionAttempts: 5,    // retry 5 times
+  timeout: 10000              // 10s connection timeout
 });
-
 
 socket.on('connect', () => {
-  console.log('connected to server with id:', socket.id)
-});
-
-socket.on('connect_error', (err) => {
-  console.error('onnection error:', err.message)
+  console.log('connected to server with ID:', socket.id);
 });
 
 socket.on('welcome', (data) => {
-  console.log('server message:', data)
+  console.log('server message:', data);
+});
+
+socket.on('connect_error', (err) => {
+  console.error('connection error:', err.message);
 });
